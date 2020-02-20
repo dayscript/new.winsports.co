675df7352a5c3cbb7a216e6ae58142ed155feee6
cvillamil <cvillamil@dayscript.com>
2020-02-20 09:50:31 -0500

    horizontal-results

--- web/modules/custom/optafeeds/components/horizontal_results/horizontal_results.js
+++ web/modules/custom/optafeeds/components/horizontal_results/horizontal_results.js
@@ -18,7 +18,7 @@ Vue.filter('capitalize', function (value) {
     }
 )
 new Vue({
-  el: '.opta-feeds-horizontal-results .whitespace-no-wrap .overflow-y-hidden',
+  el: '.opta-feeds-horizontal-results',
   data: {
     loading: 0,
     id_tournament: 371,
