ed20b903e86d71b37a221c070b24011da77dd2d2
Juan Carlos Orrego <jcorrego@gmail.com>
2020-02-24 07:03:20 -0500

    Main sections

--- web/modules/custom/optawidgets/components/opta_tournament_stats_block/opta_tournament_stats_block.js
+++ web/modules/custom/optawidgets/components/opta_tournament_stats_block/opta_tournament_stats_block.js
@@ -2,9 +2,10 @@ Vue.config.ignoredElements = ['opta-widget']
 new Vue({
     el: '.opta-tournament-stats',
     data: {
+        node: null,
         competition: '',
         season: '',
-        selected_option: 'team_ranking',
+        selected_option: 'positions',
         options: [
             {key: 'positions', label: 'Posiciones'},
             {key: 'results', label: 'Resultados'},
@@ -18,8 +19,11 @@ new Vue({
         ],
     },
     beforeMount () {
-        if(drupalSettings.opta.competition) this.competition = drupalSettings.opta.competition
-        if(drupalSettings.opta.season) this.season = drupalSettings.opta.season
+        this.node = drupalSettings.pdb.contexts['entity:node'];
+        this.competition = this.node['field_opta_id'][0]['value'];
+        this.season = this.node['field_opta_season'][0]['value'];
+        // if(drupalSettings.opta.competition) this.competition = drupalSettings.opta.competition
+        // if(drupalSettings.opta.season) this.season = drupalSettings.opta.season
     },
     methods:{
         selectOption (option_key) {
