6559094f673221925ad1ad724fbf68e45d6b8640
Juan Carlos Orrego <jcorrego@gmail.com>
2020-02-28 15:31:31 -0500

    Formations widget

--- web/modules/custom/optawidgets/components/opta_match_timeline_block/opta_match_timeline_block.js
+++ web/modules/custom/optawidgets/components/opta_match_timeline_block/opta_match_timeline_block.js
@@ -10,7 +10,6 @@ new Vue({
     beforeMount () {
         const id = this.$el.id
         this.node = drupalSettings.pdb.contexts['entity:node'];
-        console.log(drupalSettings)
         if(this.node['field_opta_id'][0]['value']) this.competition = this.node['field_opta_id'][0]['value']
         if(this.node['field_opta_season'][0]['value']) this.season = this.node['field_opta_season'][0]['value']
         if(this.node['field_opta_match_id'][0]['value']) this.match = this.node['field_opta_match_id'][0]['value']
