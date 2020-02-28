4e58f12060e3f02bac80233b8dfac5f744d8942f
Juan Carlos Orrego <jcorrego@gmail.com>
2020-02-28 15:13:07 -0500

    Display widget

--- web/modules/custom/optawidgets/components/opta_match_timeline_block/opta_match_timeline_block.js
+++ web/modules/custom/optawidgets/components/opta_match_timeline_block/opta_match_timeline_block.js
@@ -10,6 +10,7 @@ new Vue({
     beforeMount () {
         const id = this.$el.id
         this.node = drupalSettings.pdb.contexts['entity:node'];
+        console.log(drupalSettings)
         if(this.node['field_opta_tournament_id'][0]['value']) this.competition = this.node['field_opta_tournament_id'][0]['value']
         if(this.node['field_opta_season'][0]['value']) this.season = this.node['field_opta_season'][0]['value']
         if(this.node['field_id_opta'][0]['value']) this.match = this.node['field_id_opta'][0]['value']
