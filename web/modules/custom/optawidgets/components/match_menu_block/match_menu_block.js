3423334c981087ef97b500277af753687c2e5853
Juan Carlos Orrego <jcorrego@gmail.com>
2020-03-05 13:56:50 -0500

    Match tabs

--- web/modules/custom/optawidgets/components/match_menu_block/match_menu_block.js
+++ web/modules/custom/optawidgets/components/match_menu_block/match_menu_block.js
@@ -13,18 +13,25 @@ new Vue({
   beforeMount () {
     const id = this.$el.id
     this.node = drupalSettings.pdb.contexts['entity:node'];
-    if (this.node['field_opta_id'][0]['value']) {
-      this.competition = this.node['field_opta_id'][0]['value']
-    }
-    if (this.node['field_opta_season'][0]['value']) {
-      this.season = this.node['field_opta_season'][0]['value']
-    }
-    if (this.node['field_opta_match_id'][0]['value']) {
-      this.match = this.node['field_opta_match_id'][0]['value']
-    }
-
   },
   mounted () {
+    if(this.node['type'][0]['target_id'] === 'article'){
+      if (this.node['field_tipo_de_articulo'][0]['target_id'] === '2876'){
+        this.selected_option = 'cronica'
+      } else if (this.node['field_tipo_de_articulo'][0]['target_id'] === '2882'){
+        this.selected_option = 'previa'
+      }
+    } else {
+      if (this.node['field_opta_id'][0]['value']) {
+        this.competition = this.node['field_opta_id'][0]['value']
+      }
+      if (this.node['field_opta_season'][0]['value']) {
+        this.season = this.node['field_opta_season'][0]['value']
+      }
+      if (this.node['field_opta_match_id'][0]['value']) {
+        this.match = this.node['field_opta_match_id'][0]['value']
+      }
+    }
     if (this.match) {
       this.loadArticles(this.match)
     }
