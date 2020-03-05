c235e2cf347903f4d054b33d169ec79e379994b5
Juan Carlos Orrego <jcorrego@gmail.com>
2020-03-05 11:06:38 -0500

    Horizontal widget links

--- web/modules/custom/optafeeds/components/horizontal_results/horizontal_results.js
+++ web/modules/custom/optafeeds/components/horizontal_results/horizontal_results.js
@@ -73,8 +73,7 @@ new Vue({
       )
     },
     loadCronicle (match_id) {
-      new_match_id = 2049215
-      axios.get('https://test.winsports.co/api/match/cronica/' + new_match_id).then(
+      axios.get('https://test.winsports.co/api/match/cronica/' + match_id).then(
           ({data}) => {
             if (data.length > 0) {
               if (data[0].nid) {
