43e2c085963d4f0aa60009e24a28addbe2283237
Juan Carlos Orrego <jcorrego@gmail.com>
2020-03-06 17:44:13 +0000

    server files

--- web/themes/custom/tailwindcss/src/js/tailwind.js
+++ web/themes/custom/tailwindcss/src/js/tailwind.js
@@ -9,7 +9,25 @@
    */
   Drupal.behaviors.tailwindcss = {
    attach: function (context, settings) {
-     //jQuery('#block-publicidad1').insertAfter('#block-positionstableswidget');
+     relocateView();
+     $(window).resize(function() {
+       relocateView();
+     });
+     function relocateView() {
+       if($(window).width() < 640) {
+         $('#block-publicidad1').insertAfter('#block-positionstableswidget');
+         $('#block-publicidad1').addClass('tw-mt-4 tw-pt-8');
+         //$('.block-views-publicaciones-por-perfil-block').insertAfter(".node-perfil");
+         //$('.contextual-links-region').css('position', 'unset');
+         //$('.bloque-lateral').css({'width':'310px', 'float':'none', 'margin-left':'0'});
+       }else{
+         $('#block-publicidad1').insertBefore('#block-blocktabswidgetprogramacion');
+         $('#block-publicidad1').removeClass('tw-mt-4 tw-pt-8');
+         //$('.block-views-publicaciones-por-perfil-block').insertBefore(".node-perfil");
+         //$('.contextual-links-region').attr('style', '');
+         //$('.bloque-lateral').attr('style', '');
+       }
+     }
    }
-  };
+ };
 })(jQuery, Drupal);
