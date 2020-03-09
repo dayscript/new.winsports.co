e4c07104684ba3decc3e9cba5cbfbe50fae504a7
yruizday <yruiz@dayscript.com>
2020-03-09 15:23:56 -0500

    opinion

---
+++ web/modules/custom/custom_authors/src/Plugin/views/filter/ListAuthorsBlogs.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\custom_authors\Plugin\views\filter\ListAuthorsBlogs.
+ */
+
+namespace Drupal\custom_authors\Plugin\views\filter;
+
+use Drupal\views\Plugin\views\display\DisplayPluginBase;
+use Drupal\views\Plugin\views\filter\InOperator;
+use Drupal\views\ViewExecutable;
+
+/**
+ * Filters by given list of node title options.
+ *
+ * @ingroup views_filter_handlers
+ *
+ * @ViewsFilter("custom_authors_list_authors_blogs")
+ */
+class ListAuthorsBlogs extends InOperator {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function init(ViewExecutable $view, DisplayPluginBase $display, array &$options = NULL) {
+    parent::init($view, $display, $options);
+    $this->valueTitle = t('Authors');
+    $this->definition['options callback'] = array($this, 'generateOptions');
+  }
+
+  /**
+   * Override the query so that no filtering takes place if the user doesn't
+   * select any options.
+   */
+  public function query() {
+    if (!empty($this->value)) {
+       parent::query();
+    }
+  }
+
+  /**
+   * Skip validation if no options have been chosen so we can use it as a
+   * non-filter.
+   */
+  public function validate() {
+    if (!empty($this->value)) {
+      parent::validate();
+    }
+  }
+
+  /**
+   * Helper function that generates the options.
+   * @return array
+   */
+  public function generateOptions() {
+    return array(
+      '170588' => 'Jesús Niño',
+      '8349' => 'Jorge Puerto Ladino',
+      '33706' => 'Juan Moreno',
+      '139563' => 'Ricardo Moreno',
+    );
+  }
+
+}
