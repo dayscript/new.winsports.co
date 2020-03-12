cc851e354770a73da67c925ae59323bcc429c684
Juan Carlos Orrego <jcorrego@gmail.com>
2020-03-12 17:17:37 -0500

    Migration update

--- web/modules/custom/winsportsmigrate/src/Controller/MigrateController.php
+++ web/modules/custom/winsportsmigrate/src/Controller/MigrateController.php
@@ -748,7 +748,7 @@ class MigrateController {
   }
   public function attachTournaments($node, $tournaments) {
     $node->field_torneo_node = [];
-    $ignore = ['Otros Deportes'];
+    $ignore = ['Otros Deportes','Fútbol Internacional'];
     foreach (explode(',', $tournaments) as $tour_name) {
       if (trim($tour_name) == '' || in_array(trim($tour_name),$ignore)) {
         continue;
