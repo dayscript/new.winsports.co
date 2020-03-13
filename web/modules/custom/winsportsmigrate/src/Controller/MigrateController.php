4c7ec0de572514e72d080c138bf19eeb9606bafb
Juan Carlos Orrego <jcorrego@gmail.com>
2020-03-12 23:11:45 -0500

    Migration update

--- web/modules/custom/winsportsmigrate/src/Controller/MigrateController.php
+++ web/modules/custom/winsportsmigrate/src/Controller/MigrateController.php
@@ -760,6 +760,7 @@ class MigrateController {
       if($tour_name == 'No es Fútbol es LaLiga') $tour_name = 'LaLiga España 2019-2020';
       if($tour_name == 'Copa BetPlay DIMAYOR') $tour_name = 'Copa BetPlay Dimayor 2020';
       if($tour_name == 'Liga Aguila') $tour_name = 'Liga Aguila 2019-II';
+      if($tour_name == 'Torneo Aguila') $tour_name = 'Torneo Aguila 2019-II';
       $query = \Drupal::entityQuery('node');
       $query->condition('title', $tour_name);
       $query->condition('type', 'torneo');
