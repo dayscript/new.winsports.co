29b861f26f19970a9de37919590d91408ba3f655
Juan Carlos Orrego <jcorrego@gmail.com>
2020-03-17 15:07:43 -0500

    Default tournament

--- web/modules/custom/optawidgets/components/opta_team_compare_block/opta_team_compare_block.js
+++ web/modules/custom/optawidgets/components/opta_team_compare_block/opta_team_compare_block.js
@@ -2,16 +2,14 @@ Vue.config.ignoredElements = ['opta-widget']
 new Vue({
     el: '.opta-team-compare',
     data: {
-        competition: '',
-        season: '',
+        competition: '371',
+        season: '2020',
         team: '',
         node: null
     },
     beforeMount () {
         const id = this.$el.id
         this.node = drupalSettings.pdb.contexts['entity:node'];
-        if(this.node['field_opta_id'][0]['value']) this.competition = this.node['field_opta_id'][0]['value']
-        if(this.node['field_opta_season'][0]['value']) this.season = this.node['field_opta_season'][0]['value']
-        if(this.node['field_opta_match_id'][0]['value']) this.match = this.node['field_opta_match_id'][0]['value']
+        if(this.node['field_opta_id'][0]['value']) this.team = this.node['field_opta_id'][0]['value']
     }
 });
