9f797d1ca3c7a8cee6e84780a98b80bc4479ea95
Juan Carlos Orrego <jcorrego@gmail.com>
2020-03-18 12:37:31 -0500

    Opta id

--- web/modules/custom/optawidgets/components/opta_player_widgets_block/opta_player_widgets_block.js
+++ web/modules/custom/optawidgets/components/opta_player_widgets_block/opta_player_widgets_block.js
@@ -10,6 +10,6 @@ new Vue({
     beforeMount () {
         const id = this.$el.id
         this.node = drupalSettings.pdb.contexts['entity:node'];
-        if(this.node['field_opta_id'][0]['value']) this.team = this.node['field_opta_id'][0]['value']
+        if(this.node['field_opta_id'][0]['value']) this.player = this.node['field_opta_id'][0]['value']
     }
 });
