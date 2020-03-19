0673ab43cd40fe9ffd583ea5134144cb0f2a986a
Juan Carlos Orrego <jcorrego@gmail.com>
2020-03-19 01:05:09 -0500

    Team player data

--- web/modules/custom/optawidgets/components/opta_player_widgets_block/opta_player_widgets_block.js
+++ web/modules/custom/optawidgets/components/opta_player_widgets_block/opta_player_widgets_block.js
@@ -5,11 +5,13 @@ new Vue({
         competition: '371',
         season: '2020',
         player: '',
+        team: '',
         node: null
     },
     beforeMount () {
         const id = this.$el.id
         this.node = drupalSettings.pdb.contexts['entity:node'];
         if(this.node['field_opta_id'][0]['value']) this.player = this.node['field_opta_id'][0]['value']
+        if(this.node['field_opta_team'][0]['value']) this.team = this.node['field_opta_team'][0]['value']
     }
 });
