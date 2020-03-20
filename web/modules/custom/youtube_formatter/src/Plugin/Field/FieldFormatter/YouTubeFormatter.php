3c29c27444c11b6566f183f9acfd1777b997e359
Juan Carlos Orrego <jcorrego@gmail.com>
2020-03-20 08:00:34 -0500

    Youtube formatter

---
+++ web/modules/custom/youtube_formatter/src/Plugin/Field/FieldFormatter/YouTubeFormatter.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\youtube_formatter\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Field\FieldItemListInterface;
+
+/**
+ * Plugin implementation of the 'youtube_formatter' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "youtube_formatter",
+ *   label = @Translation("Youtube player formatter"),
+ *   field_types = {
+ *     "text",
+ *     "string",
+ *     "link"
+ *   }
+ * )
+ */
+class YouTubeFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $view_mode = $this->getSetting('view_mode');
+    $view_mode = $view_mode == 'home' ? 'video_player' : 'default';
+    $image     = $items->getParent()->getValue()->field_image->view($view_mode);
+    $nodeid    = $items->getParent()->getValue()->id();
+    $elements  = [];
+    foreach ($items as $delta => $item) {
+      $url              = $item->value;
+      $url              = str_replace('https://youtu.be/', 'http://www.youtube.com/embed/', $url);
+      $elements[$delta] = [
+        '#theme'     => 'youtube_player',
+        '#item'      => $item,
+        '#image'     => $image,
+        '#url'       => $url,
+        '#view_mode' => $view_mode,
+      ];
+      if (!empty($item->_attributes)) {
+        $elements[$delta]['#attributes'] += $item->_attributes;
+        unset($item->_attributes);
+      }
+      $elements[$delta]['#attached']['drupalSettings']['view_mode'] = $view_mode;
+      $elements[$delta]['#attached']['drupalSettings']['url']       = $url;
+      $elements[$delta]['#attached']['drupalSettings']['node_id']   = $nodeid;
+    }
+
+    return $elements;
+  }
+
+}
