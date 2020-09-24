a54be0950b0ee1c9227baa25290442ec51b00bf2
cvillamil <cvillamil@dayscript.com>
2020-09-24 15:39:13 -0500

    update width mediastream_fomatter

--- web/modules/custom/mediastream_formatter/js/msplayer.js
+++ web/modules/custom/mediastream_formatter/js/msplayer.js
@@ -5,7 +5,7 @@
       $('#mediastream-player-' + drupalSettings.mediastream_id + ' .image .play-icon', context).once('play-'+drupalSettings.mediastream_id).click(function() {
         var $this = $(this);
         $('.title-' + drupalSettings.node_id).hide();
-        if (width === 0) width = 854;
+        if (width === 0 || width >= 854) width = 854;
         var height = width * 9 / 16;
         var mediaStreamOptions = {
           width: width,
